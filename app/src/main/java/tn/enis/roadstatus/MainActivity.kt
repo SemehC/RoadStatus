12890bd123f2193eac7f7a07921f27c57b757157
SemehC <62266399+SemehC@users.noreply.github.com>
2021-02-22 10:07:06 +0100

    initial commit

---
+++ app/src/main/java/tn/enis/roadstatus/MainActivity.kt
@@ -0,0 +1,18 @@
+package tn.enis.roadstatus
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val bt = findViewById<Button>(R.id.start_scan_bt)
+        bt.setOnClickListener {
+            val intent = Intent(this,ScanningActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/tn/enis/roadstatus/ScanningActivity.kt
@@ -0,0 +1,11 @@
+package tn.enis.roadstatus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScanningActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scanning)
+    }
+}
